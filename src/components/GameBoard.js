60e5602142ff2530a6a6bf0b148b19bc80a8cba8
Andrew Owen <andrew.owen@willowtreeapps.com>
2018-08-30 16:25:41 -0400

    get flip behavior working

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,83 +1,123 @@
-import React from "react";
-import "./GameBoard.css";
-import EmployeeTile from "./EmployeeTile";
+import React from 'react';
+import './GameBoard.css';
+import EmployeeTile from './EmployeeTile';
+import { fetchData, shuffle } from '../helpers';
 
 class GameBoard extends React.Component {
   state = {
-    selectedEmployees: [],
-    lastFlipped: "",
-    currentlyFlipped: "",
-    numFlipped: 0
+    staff: [],
+    shuffledStaff: [],
+    matchedEmployees: [],
+    lastFlipped: '',
+    currentlyFlipped: '',
+    numFlipped: 0,
+    readyToClick: true
   };
 
   handleFlip = (details, key) => {
-    if (this.state.lastFlipped === "") {
+    console.log('clicked!');
+    // flip tile
+    const matchedEmployees = [...this.state.matchedEmployees, key];
+    // if this is the first time the game has been started, 
+    // set last flipped tile to the currently selected tile.
+    if (this.state.lastFlipped === '') {
       this.setState({
-        lastFlipped: details.firstName
+        lastFlipped: `${details.firstName} ${details.lastName}`
       });
     }
-    if (this.state.lastFlipped !== "") {
-      this.setState({
-        lastFlipped: this.state.currentlyFlipped
-      });
-    }
-    if (this.state.selectedEmployees.includes(key)) {
-      const newSelectedEmployees = [...this.state.selectedEmployees].filter(
-        foo => foo !== key
-      );
-      const newNumFlipped = this.state.numFlipped - 1;
-      this.setState({
-        selectedEmployees: newSelectedEmployees,
-        currentlyFlipped: details.firstName,
-        numFlipped: newNumFlipped
-      });
-      console.log(this.state.selectedEmployees.length);
-    } else {
-      const selectedEmployees = [...this.state.selectedEmployees, key];
-      this.setState(
-        prevState => {
-          return {
-            selectedEmployees,
-            currentlyFlipped: details.firstName,
-            numFlipped: prevState.numFlipped + 1
-          };
-        },
-        () => {
-          console.log(this.state.numFlipped);
+    this.setState(
+      prevState => {
+        return {
+          matchedEmployees,
+          currentlyFlipped: `${details.firstName} ${details.lastName}`,
+          lastFlipped: prevState.lastFlipped,
+          numFlipped: prevState.numFlipped + 1
+        };
+      },
+      () => {
+        if (this.state.numFlipped >= 2) {
+          this.setState(
+            {
+              readyToClick: false
+            },
+            () => {
+              setTimeout(() => {
+                this.handleMatch(details, key);
+              }, 1000);
+            }
+          );
         }
-      );
-    }
+      }
+    );
   };
 
-  handleMatch = (lastFlipped, currentlyFlipped) => {
-    if (lastFlipped !== currentlyFlipped) {
-      this.setState({
-        selectedEmployees: [],
-        lastFlipped: "",
-        currentlyFlipped: ""
+  handleMatch = (details, key) => {
+    const { matchedEmployees, currentlyFlipped, lastFlipped } = this.state;
+    // check if tiles match
+    // if they do, keep employees in matched array
+    if (currentlyFlipped === lastFlipped) {
+      console.log(`You found ${details.firstName} ${details.lastName}`);
+    }
+    // if they do not, remove the last two from the array (the last employees added)
+    else {
+      console.log('Try again!');
+      this.setState(prevState => {
+        const employees = [...matchedEmployees];
+        employees.splice(-2);
+        return {
+          matchedEmployees: employees
+        };
       });
     }
+    // In both cases, last flipped tile and num of tiles flipped should be reset
+    // and other tiles should be clickable again.
+    this.setState({
+      lastFlipped: '',
+      numFlipped: 0,
+      readyToClick: true
+    });
   };
 
-  resetFlipCounter = () => {
+  shuffleStaff = () => {
+    let sixRandomEmployees = shuffle(this.state.staff)
+      .slice(0, 6)
+      .map(element => {
+        return [element, element];
+      })
+      .reduce((acc, val) => acc.concat(val), []);
+    const shuffledStaff = shuffle(sixRandomEmployees);
     this.setState({
-      numFlipped: 0
+      shuffledStaff
     });
+  };
+
+  componentDidMount = async () => {
+    const staff = await fetchData(
+      'https://willowtreeapps.com/api/v1.0/profiles'
+    );
+    this.setState(
+      {
+        staff
+      },
+      this.shuffleStaff
+    );
   };
 
   render() {
     return (
       <div className="game-board-container">
-        {Object.keys(this.props.staff).map(key => (
+        {Object.keys(this.state.shuffledStaff).map((key, details) => (
           <EmployeeTile
             key={key}
             staffKey={key}
-            details={this.props.staff[key]}
-            isFlipped={this.state.selectedEmployees.indexOf(key) >= 0}
+            details={this.state.shuffledStaff[key]}
+            isFlipped={this.state.matchedEmployees.includes(key)}
             handleFlip={this.handleFlip}
             handleFlipBack={this.handleFlipBack}
+            readyToClick={this.state.readyToClick}
           />
         ))}
+        <button onClick={this.shuffleEmployees}>CLick</button>
       </div>
     );
   }
